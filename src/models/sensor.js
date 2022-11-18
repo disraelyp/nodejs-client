137f744744cee5518082c39acceaddf45448ddc0
disraelyp <disraely1911@gmail.com>
2022-11-17 20:33:01 -0400

    fechaGeneracion update

--- src/models/sensor.js
+++ src/models/sensor.js
@@ -1,6 +1,6 @@
 class Sensor {
     constructor() {
-        this.fechaGeneración = new Date();
+        this.fechaGeneración = new Date().getTime();
         this.IdDispositivo = (Math.round(Math.random() * 100)) + 1;
         this.temperatura = (Math.round(Math.random() * 100)) + 1;
         this.humedad = (Math.round(Math.random() * 100));
